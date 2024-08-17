80178a2afe9ab3837cfdb24ddedfab98533445a6
John DeSilva <desilvjo@umich.edu>
2024-08-17 06:14:41 -0400

    Genesis commit

---
+++ lib/spiaggia/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Spiaggia
+  VERSION = "0.1.0"
+end
---
+++ spec/spiaggia_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Spiaggia do
+  it "has a version number" do
+    expect(Spiaggia::VERSION).not_to be_nil
+  end
+end
